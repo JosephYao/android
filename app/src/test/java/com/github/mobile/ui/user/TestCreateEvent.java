c85fcb4a331b6ad51e7e07d2c67e0c51502dd704
josephyao <dermott@126.com>
2015-09-22 11:20:15 +0800

    add test for actor should be bold to main

--- app/src/test/java/com/github/mobile/ui/user/TestCreateEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestCreateEvent.java
@@ -3,7 +3,9 @@ package com.github.mobile.ui.user;
 import static com.github.mobile.ui.user.StyledTextDataMother.mockDetailsStyledText;
 import static com.github.mobile.ui.user.StyledTextDataMother.mockMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.util.TypefaceUtils;
 
 import org.eclipse.egit.github.core.event.Event;
@@ -11,20 +13,32 @@ import org.junit.Test;
 
 public class TestCreateEvent {
 
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+    private EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_CREATE).
+            withPayload(new CreatePayloadBuilder().defaultStubPayload());
+
     @Test
     public void icon_should_be_create() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-        Event event = new EventBuilder().
-                defaultStubEventFor(Event.TYPE_CREATE).
-                withPayload(new CreatePayloadBuilder().defaultStubPayload()).
-                build();
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                event,
+                stubEvent.build(),
                 mockMainStyledText(),
                 mockDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_CREATE, icon);
+    }
+
+    @Test
+    public void actor_should_be_bold_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForCreate").
+                        build(),
+                mockMainStyledText,
+                mockDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForCreate");
     }
 
 }
