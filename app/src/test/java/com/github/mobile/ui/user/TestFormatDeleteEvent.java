52b2bf78eac866c6b284bffa1b488e6828aca05d
josephyao <dermott@126.com>
2015-09-24 21:32:45 +0800

    add test for actor should be bold to main

--- app/src/test/java/com/github/mobile/ui/user/TestFormatDeleteEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestFormatDeleteEvent.java
@@ -3,7 +3,9 @@ package com.github.mobile.ui.user;
 import static com.github.mobile.ui.user.StyledTextDataMother.mockDetailsStyledText;
 import static com.github.mobile.ui.user.StyledTextDataMother.mockMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.util.TypefaceUtils;
 
 import org.eclipse.egit.github.core.event.Event;
@@ -11,18 +13,33 @@ import org.junit.Test;
 
 public class TestFormatDeleteEvent {
 
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+    EventBuilder stubEvent = new EventBuilder().
+            defaultStubEventFor(Event.TYPE_DELETE).
+            withPayload(new DeletePayloadBuilder().defaultStubPayload());
+
     @Test
     public void icon_should_be_delete() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().
-                        defaultStubEventFor(Event.TYPE_DELETE).
-                        withPayload(new DeletePayloadBuilder().defaultStubPayload()).
-                        build(),
+                stubEvent.build(),
                 mockMainStyledText(),
                 mockDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_DELETE, icon);
+    }
+
+
+    @Test
+    public void actor_should_be_bold_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForDelete").
+                        build(),
+                mockMainStyledText,
+                mockDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForDelete");
     }
 }
