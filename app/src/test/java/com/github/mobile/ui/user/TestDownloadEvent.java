59c191a1419bfa00494595fdb44d43b7731b774e
josephyao <dermott@126.com>
2015-09-25 21:04:58 +0800

    add test for actor upload to repo should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestDownloadEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestDownloadEvent.java
@@ -3,7 +3,9 @@ package com.github.mobile.ui.user;
 import static com.github.mobile.ui.user.StyledTextDataMother.mockDetailsStyledText;
 import static com.github.mobile.ui.user.StyledTextDataMother.mockMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.util.TypefaceUtils;
 
 import org.eclipse.egit.github.core.event.Event;
@@ -11,17 +13,34 @@ import org.junit.Test;
 
 public class TestDownloadEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_DOWNLOAD).
+            withPayload(new DownloadPayloadBuilder().defaultStubPayload());
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_upload() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_DOWNLOAD).
-                        withPayload(new DownloadPayloadBuilder().defaultStubPayload()).
-                        build(),
+                stubEvent.build(),
                 mockMainStyledText(),
                 mockDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_UPLOAD, icon);
+    }
+
+    @Test
+    public void actor_upload_to_repo_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent
+                        .withLoginUserName("LoginUserNameForDownload")
+                        .withRepo("RepoForDownload")
+                        .build(),
+                mockMainStyledText,
+                mockDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForDownload");
+        verify(mockMainStyledText).append(" uploaded a file to ");
+        verify(mockMainStyledText).bold("RepoForDownload");
     }
 }
