e2eaf91e916edbff5e9fd81bc3d8f0597dcc4e13
josephyao <dermott@126.com>
2015-09-26 21:41:54 +0800

    add test for actor and follow target should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestFollowEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestFollowEvent.java
@@ -3,7 +3,9 @@ package com.github.mobile.ui.user;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.ui.user.builder.FollowPayloadBuilder;
 import com.github.mobile.util.TypefaceUtils;
@@ -13,17 +15,35 @@ import org.junit.Test;
 
 public class TestFollowEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_FOLLOW).
+            withPayload(new FollowPayloadBuilder().defaultStubPayload());
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_follow() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_FOLLOW).
-                        withPayload(new FollowPayloadBuilder().defaultStubPayload()).
-                        build(),
+                stubEvent.build(),
                 mockMainStyledText(),
                 mockDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_FOLLOW, icon);
+    }
+
+    @Test
+    public void actor_and_follow_target_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withPayload(new FollowPayloadBuilder().defaultStubPayload().
+                            withTarget("Target")).
+                        withLoginUserName("LoginUserNameForFollow").
+                        build(),
+                mockMainStyledText,
+                mockDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForFollow");
+        verify(mockMainStyledText).append(" started following ");
+        verify(mockMainStyledText).bold("Target");
     }
 }
--- app/src/test/java/com/github/mobile/ui/user/builder/FollowPayloadBuilder.java
+++ app/src/test/java/com/github/mobile/ui/user/builder/FollowPayloadBuilder.java
@@ -1,18 +1,35 @@
 package com.github.mobile.ui.user.builder;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.eclipse.egit.github.core.User;
 import org.eclipse.egit.github.core.event.EventPayload;
 import org.eclipse.egit.github.core.event.FollowPayload;
 
 public class FollowPayloadBuilder implements PayloadBuilder {
+    private String target;
+
     @Override
     public EventPayload build() {
         FollowPayload stubPayload = mock(FollowPayload.class);
+        User stubUser = stubUser();
+        when(stubPayload.getTarget()).thenReturn(stubUser);
         return stubPayload;
     }
 
+    private User stubUser() {
+        User stubUser = mock(User.class);
+        when(stubUser.getLogin()).thenReturn(target);
+        return stubUser;
+    }
+
     public FollowPayloadBuilder defaultStubPayload() {
+        return this;
+    }
+
+    public FollowPayloadBuilder withTarget(String target) {
+        this.target = target;
         return this;
     }
 }
