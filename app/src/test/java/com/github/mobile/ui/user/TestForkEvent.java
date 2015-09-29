3547b118dda177f76c8021997571195a42207482
josephyao <dermott@126.com>
2015-09-29 13:19:06 +0800

    add test for icon should be fork

---
+++ app/src/test/java/com/github/mobile/ui/user/TestForkEvent.java
@@ -0,0 +1,26 @@
+package com.github.mobile.ui.user;
+
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockDetailsStyledText;
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
+import static org.junit.Assert.assertEquals;
+
+import com.github.mobile.ui.user.builder.EventBuilder;
+import com.github.mobile.util.TypefaceUtils;
+
+import org.eclipse.egit.github.core.event.Event;
+import org.junit.Test;
+
+public class TestForkEvent {
+
+    @Test
+    public void icon_should_be_fork() {
+        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
+        String icon = iconAndViewTextManager.setIconAndFormatStyledText(
+                new EventBuilder().defaultStubEventFor(Event.TYPE_FORK).build(),
+                mockMainStyledText(),
+                mockDetailsStyledText());
+
+        assertEquals(TypefaceUtils.ICON_FORK, icon);
+    }
+}
