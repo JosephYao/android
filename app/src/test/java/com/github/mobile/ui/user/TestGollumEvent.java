6f6a73bffcd5c544872d5ef74a318f542bdb2a8a
josephyao <dermott@126.com>
2015-09-29 17:45:56 +0800

    add test for actor and repo should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestGollumEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestGollumEvent.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.util.TypefaceUtils;
 
@@ -22,5 +25,23 @@ public class TestGollumEvent {
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_WIKI, icon);
+    }
+
+    @Test
+    public void actor_and_repo_should_be_bold_and_appended_to_main() {
+        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                new EventBuilder().defaultStubEventFor(Event.TYPE_GOLLUM).
+                        withLoginUserName("LoginUserNameForWiki").
+                        withRepo("RepoForWiki").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForWiki");
+        verify(mockMainStyledText).append(" updated the wiki in ");
+        verify(mockMainStyledText).bold("RepoForWiki");
     }
 }
