9eee59645ab27b18c2f86b49167eaa56bd028d69
josephyao <dermott@126.com>
2015-09-29 14:06:14 +0800

    add test for actor should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestGistEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestGistEvent.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.util.TypefaceUtils;
 
@@ -12,10 +15,10 @@ import org.junit.Test;
 
 public class TestGistEvent {
 
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_gist() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
                 new EventBuilder().defaultStubEventFor(Event.TYPE_GIST).
                         withPayload(new GistPayloadBuilder().defaultStubPayload()).
@@ -24,5 +27,21 @@ public class TestGistEvent {
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_GIST, icon);
+    }
+
+    @Test
+    public void actor_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                new EventBuilder().defaultStubEventFor(Event.TYPE_GIST).
+                        withPayload(new GistPayloadBuilder().defaultStubPayload()).
+                        withLoginUserName("LoginUserNameForGist").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForGist");
+        verify(mockMainStyledText).append(' ');
     }
 }
