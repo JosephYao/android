4af409149def815e332d404eada9e4c26f43d283
josephyao <dermott@126.com>
2015-09-29 13:26:02 +0800

    add test for actor and repo should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestForkEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestForkEvent.java
@@ -3,7 +3,9 @@ package com.github.mobile.ui.user;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.util.TypefaceUtils;
 
@@ -12,15 +14,33 @@ import org.junit.Test;
 
 public class TestForkEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_FORK);
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_fork() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_FORK).build(),
+                stubEvent.build(),
                 mockMainStyledText(),
                 mockDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_FORK, icon);
+    }
+
+    @Test
+    public void actor_and_repo_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForFork").
+                        withRepo("RepoForFork").
+                        build(),
+                mockMainStyledText,
+                mockDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForFork");
+        verify(mockMainStyledText).append(" forked repository ");
+        verify(mockMainStyledText).bold("RepoForFork");
     }
 }
