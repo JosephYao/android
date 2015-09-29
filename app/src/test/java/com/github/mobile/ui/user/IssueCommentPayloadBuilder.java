64471c66d7cdefb04df57cf827fcfa4bd3beb334
josephyao <dermott@126.com>
2015-09-29 21:29:21 +0800

    add test for actor should be bold and appended to main and issue should be bold to main

--- app/src/test/java/com/github/mobile/ui/user/IssueCommentPayloadBuilder.java
+++ app/src/test/java/com/github/mobile/ui/user/IssueCommentPayloadBuilder.java
@@ -6,23 +6,55 @@ import static org.mockito.Mockito.when;
 import com.github.mobile.ui.user.builder.PayloadBuilder;
 
 import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.PullRequest;
 import org.eclipse.egit.github.core.event.EventPayload;
 import org.eclipse.egit.github.core.event.IssueCommentPayload;
 
 public class IssueCommentPayloadBuilder implements PayloadBuilder {
     private Integer issueNumber;
+    private boolean withPullRequest;
 
     @Override
     public EventPayload build() {
         IssueCommentPayload stubPayload = mock(IssueCommentPayload.class);
-        Issue stubIssue = mock(Issue.class);
-        when(stubIssue.getNumber()).thenReturn(issueNumber);
+        Issue stubIssue = stubIssue();
         when(stubPayload.getIssue()).thenReturn(stubIssue);
         return stubPayload;
     }
 
+    private Issue stubIssue() {
+        Issue stubIssue = mock(Issue.class);
+        when(stubIssue.getNumber()).thenReturn(issueNumber);
+        stubPullRequest(stubIssue);
+        return stubIssue;
+    }
+
+    private void stubPullRequest(Issue stubIssue) {
+        if (withPullRequest) {
+            PullRequest stubPullRequest = mock(PullRequest.class);
+            when(stubPullRequest.getHtmlUrl()).thenReturn("HtmlUrl");
+            when(stubIssue.getPullRequest()).thenReturn(stubPullRequest);
+        }
+    }
+
     public IssueCommentPayloadBuilder defaultStubPayload() {
+        this.withPullRequest = false;
         this.issueNumber = 1;
+        return this;
+    }
+
+    public IssueCommentPayloadBuilder withPullRequest() {
+        this.withPullRequest = true;
+        return this;
+    }
+
+    public IssueCommentPayloadBuilder withIssueNumber(int issueNumber) {
+        this.issueNumber = issueNumber;
+        return this;
+    }
+
+    public IssueCommentPayloadBuilder withOutPullRequest() {
+        this.withPullRequest = false;
         return this;
     }
 }
--- app/src/test/java/com/github/mobile/ui/user/TestIssueCommentEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestIssueCommentEvent.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.util.TypefaceUtils;
 
@@ -12,17 +15,60 @@ import org.junit.Test;
 
 public class TestIssueCommentEvent {
 
+    public static final int ISSUE_NUMBER = 1;
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_ISSUE_COMMENT).
+            withPayload(new IssueCommentPayloadBuilder().defaultStubPayload());
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+    private final StyledText mockMainStyledText = mockMainStyledText();
+
     @Test
     public void icon_should_be_issue_comment() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_ISSUE_COMMENT).
-                        withPayload(new IssueCommentPayloadBuilder().defaultStubPayload()).
-                        build(),
+                stubEvent.build(),
                 stubMainStyledText(),
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_ISSUE_COMMENT, icon);
+    }
+
+    @Test
+    public void actor_should_be_bold_and_appended_to_main() {
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForIssueComment").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForIssueComment");
+        verify(mockMainStyledText).append(" commented on ");
+    }
+
+    @Test
+    public void pull_request_issue_should_be_bold_to_main() {
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.withPayload(stubIssueCommentPayloadWithIssueNumber(ISSUE_NUMBER).
+                        withPullRequest()).
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("pull request " + ISSUE_NUMBER);
+    }
+
+    @Test
+    public void other_issue_should_be_bold_to_main() {
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.withPayload(stubIssueCommentPayloadWithIssueNumber(ISSUE_NUMBER).
+                        withOutPullRequest()).
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("issue " + ISSUE_NUMBER);
+    }
+
+    private IssueCommentPayloadBuilder stubIssueCommentPayloadWithIssueNumber(int issueNumber) {
+        return new IssueCommentPayloadBuilder().withIssueNumber(issueNumber);
     }
 }
