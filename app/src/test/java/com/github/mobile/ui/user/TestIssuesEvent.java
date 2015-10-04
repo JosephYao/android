596bfd389379e119225cc9d7f1ad06dddda7ff56
josephyao <dermott@126.com>
2015-10-04 15:55:25 +0800

    add test for actor should be bold to main

--- app/src/test/java/com/github/mobile/ui/user/TestIssuesEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestIssuesEvent.java
@@ -1,10 +1,13 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.util.TypefaceUtils;
 
@@ -54,6 +57,22 @@ public class TestIssuesEvent {
 
         assertNull(icon);
     }
+
+    @Test
+    public void actor_should_be_bold_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                new EventBuilder().defaultStubEventFor(Event.TYPE_ISSUES).
+                        withPayload(new IssuesPayloadBuilder().defaultStubPayload()).
+                        withLoginUserName("LoginUserNameForIssues").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForIssues");
+    }
+
     private Event stubEventWithIssueAction(String action) {
         return new EventBuilder().defaultStubEventFor(Event.TYPE_ISSUES).
                 withPayload(new IssuesPayloadBuilder().defaultStubPayload().
