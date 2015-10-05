fcdcf674c0908f949e2290a44ac43884f69b26d4
josephyao <dermott@126.com>
2015-10-05 13:36:41 +0800

    add test for actor should be bold to main

--- app/src/test/java/com/github/mobile/ui/user/TestPullRequestEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestPullRequestEvent.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.ui.user.builder.PullRequestPayloadBuilder;
 import com.github.mobile.util.TypefaceUtils;
@@ -13,17 +16,32 @@ import org.junit.Test;
 
 public class TestPullRequestEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_PULL_REQUEST).
+            withPayload(new PullRequestPayloadBuilder().defaultStubPayload());
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_pull_request() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_PULL_REQUEST).
-                        withPayload(new PullRequestPayloadBuilder().defaultStubPayload()).
+                stubEvent.
                         build(),
                 stubMainStyledText(),
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_PULL_REQUEST, icon);
+    }
+
+    @Test
+    public void actor_should_be_bold_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForPullRequest").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForPullRequest");
     }
 }
