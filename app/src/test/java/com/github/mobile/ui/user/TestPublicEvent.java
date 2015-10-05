e5d4b06e81347331033425afe046572cc547f371
josephyao <dermott@126.com>
2015-10-05 13:23:37 +0800

    add test for actor and repo should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestPublicEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestPublicEvent.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 
 import org.eclipse.egit.github.core.event.Event;
@@ -11,15 +14,33 @@ import org.junit.Test;
 
 public class TestPublicEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_PUBLIC);
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_null() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_PUBLIC).build(),
+                stubEvent.build(),
                 stubMainStyledText(),
                 stubDetailsStyledText());
 
         assertNull(icon);
+    }
+
+    @Test
+    public void actor_and_repo_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForPublic").
+                        withRepo("RepoForPublic").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForPublic");
+        verify(mockMainStyledText).append(" open sourced repository ");
+        verify(mockMainStyledText).bold("RepoForPublic");
     }
 }
