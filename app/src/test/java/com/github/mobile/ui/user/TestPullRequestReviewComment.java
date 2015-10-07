7120cb6d38aea7d4e06246fa10100f5cae588fac
josephyao <dermott@126.com>
2015-10-07 14:28:31 +0800

    add test for actor and repo should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestPullRequestReviewComment.java
+++ app/src/test/java/com/github/mobile/ui/user/TestPullRequestReviewComment.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.ui.user.builder.PullRequestReviewCommentPayloadBuilder;
 import com.github.mobile.util.TypefaceUtils;
@@ -13,17 +16,37 @@ import org.junit.Test;
 
 public class TestPullRequestReviewComment {
 
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_comment() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_PULL_REQUEST_REVIEW_COMMENT).
-                        withPayload(new PullRequestReviewCommentPayloadBuilder().defaultStubPayload()).
-                        build(),
+                stubEvent().build(),
                 stubMainStyledText(),
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_COMMENT, icon);
+    }
+
+    private EventBuilder stubEvent() {
+        return new EventBuilder().defaultStubEventFor(Event.TYPE_PULL_REQUEST_REVIEW_COMMENT).
+                withPayload(new PullRequestReviewCommentPayloadBuilder().defaultStubPayload());
+    }
+
+    @Test
+    public void actor_and_repo_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent().
+                        withLoginUserName("LoginUserNameForPullRequestReviewComment").
+                        withRepo("RepoForPullRequestReviewComment").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForPullRequestReviewComment");
+        verify(mockMainStyledText).append(" commented on ");
+        verify(mockMainStyledText).bold("RepoForPullRequestReviewComment");
     }
 }
