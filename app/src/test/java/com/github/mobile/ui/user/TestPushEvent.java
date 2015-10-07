3868e445c019999b24efa7c3133da2582dda40f5
josephyao <dermott@126.com>
2015-10-07 20:45:24 +0800

    add test for actor should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestPushEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestPushEvent.java
@@ -1,9 +1,12 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.ui.user.builder.PushPayloadBuilder;
 import com.github.mobile.util.TypefaceUtils;
@@ -13,17 +16,32 @@ import org.junit.Test;
 
 public class TestPushEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_PUSH).
+            withPayload(new PushPayloadBuilder().defaultStubPayload());
+    IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+
     @Test
     public void icon_should_be_push() {
-        IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
-
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_PUSH).
-                        withPayload(new PushPayloadBuilder().defaultStubPayload()).
-                        build(),
+                stubEvent.build(),
                 stubMainStyledText(),
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_PUSH, icon);
+    }
+
+    @Test
+    public void actor_should_be_bold_and_appended_to_main() {
+        StyledText mockMainStyledText = mockMainStyledText();
+
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent.
+                        withLoginUserName("LoginUserNameForPush").
+                        build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForPush");
+        verify(mockMainStyledText).append(" pushed to ");
     }
 }
