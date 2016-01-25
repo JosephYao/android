9d189eb2323a6000aa11fb204b22fbd14d182c36
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-01-25 22:13:21 +0800

    replace RepoTarget implementation with Repo from Factory

--- app/src/main/java/com/github/mobile/ui/user/target/RepoTarget.java
+++ app/src/main/java/com/github/mobile/ui/user/target/RepoTarget.java
@@ -1,35 +1,21 @@
 package com.github.mobile.ui.user.target;
 
-import android.text.TextUtils;
-
 import com.github.mobile.ui.StyledText;
+import com.github.mobile.ui.user.repo.Repo;
+import com.github.mobile.ui.user.repo.RepoFactory;
 
 import org.eclipse.egit.github.core.event.Event;
-import org.eclipse.egit.github.core.event.EventRepository;
 
 public class RepoTarget implements Target {
-    private final Event event;
+    private final Repo repo;
 
     public RepoTarget(Event event) {
-        this.event = event;
+        repo = RepoFactory.createRepoFromEventRepositoryAndRefType(event.getRepo(), "repository");
     }
 
     @Override
     public void render(StyledText text) {
-        boldRepoName(text, event);
+        repo.render(text);
     }
 
-    private StyledText boldRepoName(final StyledText text,
-            final Event event) {
-        EventRepository repo = event.getRepo();
-        if (repo != null) {
-            String name = repo.getName();
-            if (!TextUtils.isEmpty(name)) {
-                int slash = name.indexOf('/');
-                if (slash != -1 && slash + 1 < name.length())
-                    text.bold(name.substring(slash + 1));
-            }
-        }
-        return text;
-    }
 }
