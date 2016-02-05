204fef29bb730c04722c4d4a3d937fb0cf88c908
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-05 22:11:15 +0800

    add test for actor should be bold and appended to main

--- app/src/test/java/com/github/mobile/ui/user/TestWatchEvent.java
+++ app/src/test/java/com/github/mobile/ui/user/TestWatchEvent.java
@@ -1,10 +1,13 @@
 package com.github.mobile.ui.user;
 
+import static com.github.mobile.ui.user.builder.StyledTextDataMother.mockMainStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubDetailsStyledText;
 import static com.github.mobile.ui.user.builder.StyledTextDataMother.stubMainStyledText;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
 import com.github.mobile.BuildConfig;
+import com.github.mobile.ui.StyledText;
 import com.github.mobile.ui.user.builder.EventBuilder;
 import com.github.mobile.util.TypefaceUtils;
 
@@ -18,15 +21,30 @@ import org.robolectric.annotation.Config;
 @Config(constants = BuildConfig.class)
 public class TestWatchEvent {
 
+    private final EventBuilder stubEvent = new EventBuilder().defaultStubEventFor(Event.TYPE_WATCH);
     private IconAndViewTextManager iconAndViewTextManager = new IconAndViewTextManager(null);
+    private final StyledText mockMainStyledText = mockMainStyledText();
 
     @Test
     public void icon_should_be_star() {
         String icon = iconAndViewTextManager.setIconAndFormatStyledText(
-                new EventBuilder().defaultStubEventFor(Event.TYPE_WATCH).build(),
+                stubEvent.build(),
                 stubMainStyledText(),
                 stubDetailsStyledText());
 
         assertEquals(TypefaceUtils.ICON_STAR, icon);
+    }
+
+    @Test
+    public void actor_should_be_bold_and_appended_to_main() {
+        iconAndViewTextManager.setIconAndFormatStyledText(
+                stubEvent
+                        .withLoginUserName("LoginUserNameForWatch")
+                        .build(),
+                mockMainStyledText,
+                stubDetailsStyledText());
+
+        verify(mockMainStyledText).bold("LoginUserNameForWatch");
+        verify(mockMainStyledText).append(" starred ");
     }
 }
