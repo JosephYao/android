11cce10ee502baf287f2da7ff4a942195ddeda12
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-08 21:46:36 +0800

    refactor action related classes

--- app/src/main/java/com/github/mobile/ui/user/action/ActionFactory.java
+++ app/src/main/java/com/github/mobile/ui/user/action/ActionFactory.java
@@ -1,6 +1,7 @@
 package com.github.mobile.ui.user.action;
 
 import org.eclipse.egit.github.core.event.Event;
+import org.eclipse.egit.github.core.event.EventPayload;
 import org.eclipse.egit.github.core.event.GistPayload;
 import org.eclipse.egit.github.core.event.IssuesPayload;
 import org.eclipse.egit.github.core.event.PullRequestPayload;
@@ -8,24 +9,26 @@ import org.eclipse.egit.github.core.event.PullRequestPayload;
 public class ActionFactory {
 
     public static Action create(Event event) {
-        if (event.getPayload() instanceof GistPayload)
-            return createFromGistPayload((GistPayload)event.getPayload());
+        EventPayload payload = event.getPayload();
 
-        if (event.getPayload() instanceof IssuesPayload)
-            return createFromIssuesPayload((IssuesPayload) event.getPayload());
+        if (payload instanceof GistPayload)
+            return createFromGistPayload((GistPayload) payload);
 
-        return createFromPullRequestPayload((PullRequestPayload) event.getPayload());
+        if (payload instanceof IssuesPayload)
+            return createFromIssuesPayload((IssuesPayload) payload);
+
+        return createFromPullRequestPayload((PullRequestPayload) payload);
     }
 
-    public static Action createFromGistPayload(GistPayload payload) {
+    private static Action createFromGistPayload(GistPayload payload) {
         return new GistAction(payload.getAction(), payload.getGist().getId());
     }
 
-    public static Action createFromIssuesPayload(IssuesPayload payload) {
+    private static Action createFromIssuesPayload(IssuesPayload payload) {
         return new IssuesAction(payload.getAction(), payload.getIssue().getNumber());
     }
 
-    public static Action createFromPullRequestPayload(PullRequestPayload payload) {
+    private static Action createFromPullRequestPayload(PullRequestPayload payload) {
         if ("synchronize".equals(payload.getAction()))
             return new PullRequestAction("updated", payload.getNumber());
 
--- app/src/main/java/com/github/mobile/ui/user/action/GistAction.java
+++ app/src/main/java/com/github/mobile/ui/user/action/GistAction.java
@@ -3,24 +3,14 @@ package com.github.mobile.ui.user.action;
 import com.github.mobile.ui.StyledText;
 import com.github.mobile.util.TypefaceUtils;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class GistAction implements Action {
 
-    private final String action;
     private final String gistId;
-    private final Map<String, String> textToBeAppended;
+    private final String actionText;
 
     public GistAction(String action, String gistId) {
-        this.action = action;
+        this.actionText = actionText(action);
         this.gistId = gistId;
-        textToBeAppended = new HashMap<String, String>() {{
-            put("create", "created");
-            put("update", "updated");
-        }};
-        if (!textToBeAppended.containsKey(action))
-            textToBeAppended.put(action, action);
     }
 
     @Override
@@ -34,12 +24,22 @@ public class GistAction implements Action {
         return TypefaceUtils.ICON_GIST;
     }
 
-    private void renderGistId(StyledText main) {
-        main.append(" Gist ");
-        main.append(gistId);
+    private void renderGistId(StyledText text) {
+        text.append(" Gist ");
+        text.append(gistId);
     }
 
-    private void renderAction(StyledText main) {
-        main.append(textToBeAppended.get(action));
+    private void renderAction(StyledText text) {
+        text.append(actionText);
+    }
+
+    private String actionText(String action) {
+        if ("create".equals(action))
+            return "created";
+
+        if ("update".equals(action))
+            return "updated";
+
+        return action;
     }
 }
