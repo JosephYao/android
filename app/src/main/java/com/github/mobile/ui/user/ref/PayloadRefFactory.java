11bee991fe233129db7f1f33a9d75594147f47ba
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-09 15:21:19 +0800

    refactor payload ref related code

--- app/src/main/java/com/github/mobile/ui/user/ref/PayloadRefFactory.java
+++ app/src/main/java/com/github/mobile/ui/user/ref/PayloadRefFactory.java
@@ -1,44 +1,46 @@
 package com.github.mobile.ui.user.ref;
 
+import com.github.mobile.ui.user.repo.Repo;
 import com.github.mobile.ui.user.repo.RepoFactory;
 
 import org.eclipse.egit.github.core.event.CreatePayload;
 import org.eclipse.egit.github.core.event.DeletePayload;
 import org.eclipse.egit.github.core.event.Event;
-import org.eclipse.egit.github.core.event.EventRepository;
 import org.eclipse.egit.github.core.event.PushPayload;
 
 public class PayloadRefFactory {
 
     public static PayloadRef create(Event event) {
         if (event.getPayload() instanceof CreatePayload)
-            return createFromCreatePayload((CreatePayload) event.getPayload(), event.getRepo());
+            return createFromCreatePayload((CreatePayload) event.getPayload(), createRepo(event));
 
         if (event.getPayload() instanceof DeletePayload)
             return createFromDeletePayload((DeletePayload) event.getPayload());
 
-        return createFromPushPayload((PushPayload) event.getPayload());
+        return createFromPushPayloadRef(((PushPayload) event.getPayload()).getRef());
     }
 
-    public static PayloadRef createFromCreatePayload(CreatePayload payload, EventRepository repo) {
+    private static PayloadRef createFromCreatePayload(CreatePayload payload, Repo repo) {
         String refType = payload.getRefType();
 
         if (refType.equals("repository"))
-            return new RepositoryRef(refType, RepoFactory.createRepoFromRefType(repo, refType));
+            return new RepositoryRef(refType, repo);
         else
-            return new NonRepositoryRef(refType, payload.getRef(), RepoFactory.createRepoFromRefType(repo, refType));
+            return new NonRepositoryRef(refType, payload.getRef(), repo);
     }
 
-    public static PayloadRef createFromDeletePayload(DeletePayload payload) {
+    private static Repo createRepo(Event event) {
+        return RepoFactory.createRepoFromRefType(event.getRepo(), ((CreatePayload) event.getPayload()).getRefType());
+    }
+
+    private static PayloadRef createFromDeletePayload(DeletePayload payload) {
         return new DeleteRef(payload.getRefType(), payload.getRef());
     }
 
-    public static PayloadRef createFromPushPayload(PushPayload payload) {
-        String ref = payload.getRef();
-
+    private static PayloadRef createFromPushPayloadRef(String ref) {
         if (ref.startsWith("refs/heads/"))
             return new PushHeadsRef(ref);
-
-        return new PushNonHeadsRef(ref);
+        else
+            return new PushNonHeadsRef(ref);
     }
 }
