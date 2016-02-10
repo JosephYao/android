3d20edf62d28f63c41abbfe9fd01c7dc1efc85de
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-10 16:19:14 +0800

    refactor issue comment payload builder

--- app/src/test/java/com/github/mobile/ui/user/builder/IssueCommentPayloadBuilder.java
+++ app/src/test/java/com/github/mobile/ui/user/builder/IssueCommentPayloadBuilder.java
@@ -1,6 +1,5 @@
 package com.github.mobile.ui.user.builder;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.eclipse.egit.github.core.Comment;
@@ -10,37 +9,29 @@ import org.eclipse.egit.github.core.event.EventPayload;
 import org.eclipse.egit.github.core.event.IssueCommentPayload;
 
 public class IssueCommentPayloadBuilder implements PayloadBuilder {
-    private IssueBuilder issueBuilder;
-    private Integer issueNumber;
+    private IssueBuilder issueBuilder = new IssueBuilder().defaultStubIssue();
+    private Integer issueNumber = 100;
     private String comment;
-    private CommentBuilder<Comment> commentBuilder;
-    private PullRequestBuilder pullRequestBuilder;
+    private CommentBuilder<Comment> commentBuilder = new CommentBuilder<>(Comment.class);
+    private PullRequestBuilder pullRequestBuilder = new PullRequestBuilder().defaultStubPullRequest();
 
     @Override
     public EventPayload build() {
-        IssueCommentPayload stubPayload = mock(IssueCommentPayload.class);
-        stubIssueAndPullRequest(stubPayload);
-        stubComment(stubPayload);
-        return stubPayload;
+        IssueCommentPayload issueCommentPayload = new IssueCommentPayload();
+        issueCommentPayload.setIssue(stubIssue());
+        issueCommentPayload.setComment(comment());
+        return issueCommentPayload;
     }
 
-    private void stubComment(IssueCommentPayload stubPayload) {
-        Comment stubComment = commentBuilder.withComment(comment).build();
-        when(stubPayload.getComment()).thenReturn(stubComment);
+    private Comment comment() {
+        return commentBuilder.withComment(comment).build();
     }
 
-    private void stubIssueAndPullRequest(IssueCommentPayload stubPayload) {
+    private Issue stubIssue() {
         Issue stubIssue = issueBuilder.withNumber(issueNumber).build();
         PullRequest stubPullRequest = pullRequestBuilder.withHtmlUrl("HtmlUrl").build();
         when(stubIssue.getPullRequest()).thenReturn(stubPullRequest);
-        when(stubPayload.getIssue()).thenReturn(stubIssue);
-    }
-
-    public IssueCommentPayloadBuilder() {
-        issueBuilder = new IssueBuilder().defaultStubIssue();
-        commentBuilder = new CommentBuilder<>(Comment.class);
-        pullRequestBuilder = new PullRequestBuilder().defaultStubPullRequest();
-        this.issueNumber = 1;
+        return stubIssue;
     }
 
     public IssueCommentPayloadBuilder withPullRequest() {
